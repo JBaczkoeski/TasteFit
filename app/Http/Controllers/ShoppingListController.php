8d4b685aab4f458a74ed5d7a31ba9d8787fd4d4b
JBaczkowski <baczkowskikuba2@gmail.com>
2025-11-27 18:17:29 +0100

    plans save into database done

--- app/Http/Controllers/ShoppingListController.php
+++ app/Http/Controllers/ShoppingListController.php
@@ -16,44 +16,4 @@ class ShoppingListController extends Controller
     {
         return Inertia::render('ShoppingList/Index');
     }
-
-    /**
-     * Display the user's profile form.
-     */
-    public function edit(Request $request): Response
-    {
-        return Inertia::render('Profile/Edit', [
-            'mustVerifyEmail' => $request->user() instanceof MustVerifyEmail,
-            'status' => session('status'),
-        ]);
-    }
-
-    /**
-     * Update the user's profile information.
-     */
-    public function create()
-    {
-        return Inertia::render('Meal/Plans/Create');
-    }
-
-    /**
-     * Delete the user's account.
-     */
-    public function destroy(Request $request): RedirectResponse
-    {
-        $request->validate([
-            'password' => ['required', 'current_password'],
-        ]);
-
-        $user = $request->user();
-
-        Auth::logout();
-
-        $user->delete();
-
-        $request->session()->invalidate();
-        $request->session()->regenerateToken();
-
-        return Redirect::to('/');
-    }
 }
